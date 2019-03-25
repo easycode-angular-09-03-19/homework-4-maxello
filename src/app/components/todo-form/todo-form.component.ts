004288c131dbbd97a445a9dc59e22e0fade3c780
maxello <maxellort@gmail.com>
2019-03-25 11:54:50 +0200

    Added homework

---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Task } from '../../interfaces/task.interface';
+import { FormTask } from '../../interfaces/form-task.interface';
+
+@Component({
+  selector: 'app-todo-form',
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.css']
+})
+export class TodoFormComponent implements OnInit {
+  @Output() outputTask: EventEmitter<Task> = new EventEmitter<Task>();
+  @Input() lastId: number;
+  formData: FormTask = {
+    name: ""
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addTask(){
+    this.lastId += 1;
+    this.outputTask.emit({
+      id: this.lastId, 
+      text: this.formData.name,
+      completed: false
+    });
+  }
+
+  onSubmit(form: any){
+    if(form.valid){
+      this.addTask();
+      form.resetForm();
+    }
+  }
+}
